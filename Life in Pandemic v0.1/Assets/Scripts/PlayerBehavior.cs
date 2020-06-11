23b5ee404cebda7ccf8a55092e842ed44ed6d70e
Edgar <edgarv576@gmail.com>
2020-06-11 17:48:25 +0200

    Adding animations

--- Life in Pandemic v0.1/Assets/Scripts/PlayerBehavior.cs
+++ Life in Pandemic v0.1/Assets/Scripts/PlayerBehavior.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+
+    float speed = 4;
+    float rotSpeed = 80;
+    float gravity = 8;
+
+    Vector3 moveDir = Vector3.zero;
+
+    CharacterController controller;
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+
+        if (controller.isGrounded)
         {
+            if (Input.GetKey(KeyCode.W))
+            {
+                moveDir = new Vector3(0, 0, 1);
+                moveDir *= speed;
+            }
+
+
 
         }
+
+        moveDir.y -= gravity * Time.deltaTime;
+        controller.Move(moveDir * Time.deltaTime);
+        
 
         
     }
